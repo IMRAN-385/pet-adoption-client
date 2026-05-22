44bfbfcfb77e53482ca60923f78e468e282ac0c5
IMRAN-385 <imranhimel385@gmail.com>
2026-05-22 12:22:51 +0600

    fix vite

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://pet-adoption-server-slno.onrender.com/api',
   withCredentials: true,
   timeout: 12000,
 });
@@ -14,9 +14,6 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
-    // ❌ window.location.href = '/login' — এটা DELETE করো
-    // ❌ navigate('/login') — এটাও না
-    // শুধু error return করো, redirect AuthContext/PrivateRoute করবে
     return Promise.reject(error);
   }
 );
